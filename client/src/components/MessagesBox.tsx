233a1e12f682b19fa2999e08257e4cbc5214a245
RomainC75 <rom.chenard@gmail.com>
2023-01-06 21:25:36 +0100

    print messages in MessagesBox

--- client/src/components/MessagesBox.tsx
+++ client/src/components/MessagesBox.tsx
@@ -3,17 +3,22 @@ import { MessageInterface } from '../@types/message'
 
 import './styles/messagesBox.css'
 
-interface MessagesBoxInterface{
-    conversation:MessageInterface[]
-    isPublic?:boolean
+interface MessagesBoxInterface {
+  conversation: MessageInterface[]
+  isPublic?: boolean
 }
 
-const MessagesBox = ({conversation, isPublic}:MessagesBoxInterface) => {
-
+const MessagesBox = ({ conversation, isPublic }: MessagesBoxInterface) => {
   return (
     <div className="MessagesBox">
-        {conversation.map((message, index)=>
-            <div className="message" key={message._id}>{JSON.stringify(message)}</div>)}
+      {conversation.sort((a,b)=>new Date(a.createdAt).getTime()-new Date(b.createdAt).getTime()).map((message, index) => (
+        <div className={message.fromSelf ? 'message self' : 'message'}>
+          <div className="time">{`${new Date(message.createdAt).getHours()}:${new Date(message.createdAt).getMinutes()}`}</div>
+          <div className={message.fromSelf ? 'text self' : 'text'} key={message._id}>
+            <p>{message.message}</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
