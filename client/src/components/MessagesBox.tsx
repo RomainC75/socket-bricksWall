47e0a47a5dbd1cec2fa04414fbdbf2b0ccae3a54
RomainC75 <rom.chenard@gmail.com>
2023-01-10 11:07:32 +0100

    client : time display in messages Box

--- client/src/components/MessagesBox.tsx
+++ client/src/components/MessagesBox.tsx
@@ -10,14 +10,21 @@ interface MessagesBoxInterface {
 const MessagesBox = ({ conversation, isPublic }: MessagesBoxInterface) => {
   return (
     <div className="MessagesBox">
-      {conversation.sort((a,b)=>new Date(a.createdAt).getTime()-new Date(b.createdAt).getTime()).map((message, index) => (
-        <div className={message.fromSelf ? 'message self' : 'message'} key={message._id}>
-          <div className="time">{`${new Date(message.createdAt).getHours()}:${new Date(message.createdAt).getMinutes()}`}</div>
-          <div className={message.fromSelf ? 'text self' : 'text'} >
-            <p>{message.message}</p>
-          </div>
-        </div>
-      ))}
+      {conversation
+        .sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+        .map((message, index) => {
+          const fullTime = new Date(message.createdAt)
+          const hours = fullTime.getHours() < 10 ? `0${fullTime.getHours()}` : fullTime.getHours() 
+          const minutes = fullTime.getMinutes() < 10 ? `0${fullTime.getMinutes()}` : fullTime.getMinutes() 
+          return (
+            <div className={message.fromSelf ? 'message self' : 'message'} key={message._id}>
+              <div className="time">{`${hours}:${minutes}`}</div>
+              <div className={message.fromSelf ? 'text self' : 'text'}>
+                <p>{message.message}</p>
+              </div>
+            </div>
+          )
+        })}
     </div>
   )
 }
